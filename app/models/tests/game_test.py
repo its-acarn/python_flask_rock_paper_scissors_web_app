5f2cc89be6a3c803ae68cc50b36e76456ebe4030
svvishjs <andreww92@live.com>
2020-09-25 19:08:35 +0100

    amended test names to read clearer

--- app/models/tests/game_test.py
+++ app/models/tests/game_test.py
@@ -5,15 +5,16 @@ from src.game import Game
 class TestGame(unittest.TestCase):
 
     def setUp(self):
-        self.player_1 = Player("Sebastion", "Rock")
-        self.player_2 = Player("Barnaby", "Scissors")
+        self.player_rock = Player("Sebastion", "Rock")
+        self.player_paper = Player("Theobold", "Paper")
+        self.player_scissors = Player("Barnaby", "Scissors")
 
-        self.game_1 = Game(self.player_1, self.player_2)
+        self.game_1 = Game(self.player_rock, self.player_paper)
 
     def test_game_has_expected_properties(self):
-        self.assertEqual(self.player_1, self.game_1.player_1)
-        self.assertEqual(self.player_2, self.game_1.player_2)
+        self.assertEqual(self.player_rock, self.game_1.player_1)
+        self.assertEqual(self.player_paper, self.game_1.player_2)
 
-    def test_rock_paper_scissors_logic___player_1_wins_with__rock(self):
-        result = self.game_1.rock_paper_scissors_logic(self.player_1.choice, self.player_2.choice)
-        self.assertEqual("Player 1 wins",result)
+    def test_rock_paper_scissors_logic___player_1_wins_with__rock_v_scissors(self):
+        result = self.game_1.rock_paper_scissors_logic(self.player_rock.choice, self.player_scissors.choice)
+        self.assertEqual("Player 1 wins", result)
