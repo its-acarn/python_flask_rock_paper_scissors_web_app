a7c6d063d9bcb2fd23db01ee75cde5fb27987b95
svvishjs <andreww92@live.com>
2020-09-25 19:03:00 +0100

    added function to run rock/paper/scissors game and one test so far. Further tests to come.

--- app/models/src/game.py
+++ app/models/src/game.py
@@ -2,6 +2,14 @@ class Game():
 
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
-        self.player_2 =player_2
+        self.player_2 = player_2
 
-    # def rock_paper_scissors_logic(self, player_1_choice, player_2_choice):
+    def rock_paper_scissors_logic(self, player_1_choice, player_2_choice):
+        if player_1_choice == player_2_choice:
+            return "Draw"
+
+        if (player_1_choice == "Rock" and player_2_choice == "Scissors") or (player_1_choice == "Paper" and player_2_choice == "Rock") or (player_1_choice == "Scissors" and player_2_choice == "Paper"):
+            return "Player 1 wins"
+
+        if (player_2_choice == "Rock" and player_1_choice == "Scissors") or (player_2_choice == "Paper" and player_1_choice == "Rock") or (player_2_choice == "Scissors" and player_1_choice == "Paper"):
+            return "Player 2 wins" 
--- app/models/tests/game_test.py
+++ app/models/tests/game_test.py
@@ -13,3 +13,7 @@ class TestGame(unittest.TestCase):
     def test_game_has_expected_properties(self):
         self.assertEqual(self.player_1, self.game_1.player_1)
         self.assertEqual(self.player_2, self.game_1.player_2)
+
+    def test_rock_paper_scissors_logic___player_1_wins_with__rock(self):
+        result = self.game_1.rock_paper_scissors_logic(self.player_1.choice, self.player_2.choice)
+        self.assertEqual("Player 1 wins",result)
