a402e7147cba4557b32e2228f6d1d82bbc76b9a9
svvishjs <andreww92@live.com>
2020-09-25 19:10:50 +0100

    added tests for player 1 win with paper v rock

--- app/models/tests/game_test.py
+++ app/models/tests/game_test.py
@@ -11,10 +11,22 @@ class TestGame(unittest.TestCase):
 
         self.game_1 = Game(self.player_rock, self.player_paper)
 
+
     def test_game_has_expected_properties(self):
         self.assertEqual(self.player_rock, self.game_1.player_1)
         self.assertEqual(self.player_paper, self.game_1.player_2)
 
+
     def test_rock_paper_scissors_logic___player_1_wins_with__rock_v_scissors(self):
         result = self.game_1.rock_paper_scissors_logic(self.player_rock.choice, self.player_scissors.choice)
+        self.assertEqual("Player 1 wins", result)
+
+
+    def test_rock_paper_scissors_logic___player_1_wins_with__paper_v_rock(self):
+        result = self.game_1.rock_paper_scissors_logic(self.player_paper.choice, self.player_rock.choice)
+        self.assertEqual("Player 1 wins", result)
+
+
+    def test_rock_paper_scissors_logic___player_1_wins_with__scissors_v_paper(self):
+        result = self.game_1.rock_paper_scissors_logic(self.player_scissors.choice, self.player_paper.choice)
         self.assertEqual("Player 1 wins", result)
