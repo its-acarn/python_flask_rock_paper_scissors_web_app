3a45ad596365590862dab5653e29cd8f1823249d
svvishjs <andreww92@live.com>
2020-09-26 16:02:51 +0100

    added a html docs for welcome, play and result pages. Also added routes to controller for these pages. No python logic added to html/flask yet to run game

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
 FLASK_APP=rps_app.py
-FLASK_ENV==development
+FLASK_ENV=development
--- app/controller.py
+++ app/controller.py
@@ -1,19 +1,34 @@
 from flask import Flask, flash, redirect, render_template, request, url_for
 from app import app
 
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('play'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'secret':
-            error = 'Invalid credentials'
-        else:
-            flash('You were successfully logged in')
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome_page.html')
+
+@app.route('/play')
+def play():
+    return render_template('play_page.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+
+    print(request.form)
+    return "Done"
+    # return render_template('result_page.html')
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != 'admin' or request.form['password'] != 'secret':
+#             error = 'Invalid credentials'
+#         else:
+#             flash('You were successfully logged in')
+#             return redirect(url_for('index'))
+#     return render_template('login.html', error=error)
